712f5485ae9325cbc87c420770db55f5958bae67
dynexcel-ddc1 <ddc1@dynexce.co>
2022-02-05 14:21:31 +0500

    update  expenses

--- de_employee_enhancement/models/employee_enhancement.py
+++ de_employee_enhancement/models/employee_enhancement.py
@@ -9,6 +9,16 @@ from odoo.exceptions import UserError
 class EmployeeEnhancement(models.Model):
     _inherit = 'hr.employee'
 
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        if name:
+            # Be sure name_search is symetric to name_get
+            name = name.split(' / ')[-1]
+            args = ['|', ('name',operator, name),('emp_number', operator , name)] + args
+        return self._search(args, limit=limit, access_rights_uid=name_get_uid)
+
+
     emp_number = fields.Char('Employee Number')
     emp_status = fields.Char('Employee Status')
     emp_type = fields.Selection([
